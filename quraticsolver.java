c42f99d5c9fc984d0c993726023b7dd3b3285f8e
Joshua Kuehn <kuehnjt@umich.edu>
2024-03-07 14:35:03 -0500

    Third Commit

--- quraticsolver.java
+++ quraticsolver.java
@@ -5,13 +5,13 @@ public class quraticsolver {
 		// TODO Auto-generated method stub
 		int a = 1;
 		int b = 3;
-		int d = 2;
-		if (b*b - 4*a*d <0)
-			System.out.println("This equation " + a + "x^2 + " + b +"x + " + d +" has no solution");
+		int e = 2;
+		if (b*b - 4*a*e <0)
+			System.out.println("This equation " + a + "x^2 + " + b +"x + " + e +" has no solution");
 		else
 			{
-				System.out.println("The solution for the equation " + a + "x^2 + " + b +"x + " + d +" are");
-				System.out.println((-b+Math.sqrt(b*b-4*a*d))/2*a + " and " + (-b-Math.sqrt(b*b-4*a*d))/2*a);
+				System.out.println("The solution for the equation " + a + "x^2 + " + b +"x + " + e +" are");
+				System.out.println((-b+Math.sqrt(b*b-4*a*e))/2*a + " and " + (-b-Math.sqrt(b*b-4*a*e))/2*a);
 			}
 	}
 
